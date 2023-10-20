37b46604ae05e71eece02b5a8cc5a8e276dda6da
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-20 17:17:12 +0200

    add others products

--- db.php
+++ db.php
@@ -5,11 +5,17 @@ $prodotti = [];
 array_push(
     $prodotti,
 
-    $biscotti = new Food('biscotti', 60, new Category('Gatti'), "https://unsplash.it/300/300?image=12", 45),
+    $biscotti = new Food('biscotti lusso', 60, new Category('Gatti'), "https://unsplash.it/300/300?image=12", 45),
 
     $rana = new Toy('rana gomma', 44, new Category('Cani'), "https://unsplash.it/300/300?image=20", 'gomma'),
 
-    $trasportino = new Kennel('cuccia comoda', 100, new Category('gatti'), "https://unsplash.it/300/300?image=44", 50, 40, 20),
+    $trasportino = new Kennel('trasportino', 100, new Category('gatti'), "https://unsplash.it/300/300?image=44", 50, 40, 20),
+
+    $crocchette = new Food('crocchette per gatti', 33, new Category('gatti'), "https://unsplash.it/300/300?image=142", 100),
+
+    $palla = new Toy('palla per gioco', 23, new Category('cani'), "https://unsplash.it/300/300?image=242", 'plastica'),
+
+    $cuccia_cane = new Kennel('cuccia da casa', 19, new Category('cani'), "https://unsplash.it/300/300?image=344", 40, 20, 40)
 
     /* place here new product */
 );
